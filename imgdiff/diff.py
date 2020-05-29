f08baaced311698498700d4b82c429eb129fb4ca
RealNameHidden <dheeraj.as008@gmail.com>
2020-05-29 18:27:19 -0400

    completed documentation

--- imgdiff/diff.py
+++ imgdiff/diff.py
@@ -1,18 +1,35 @@
 import csv
+import os
 import sys
 import time
+
 import dhash
-import os
 from PIL import Image
 
 """
-This is pydoc
+imgdiff main module
+This is Module reads the csv file and process the images and determines similarity score. Finally a cv file is generated.
 """
+
+
 def diff(h1, h2):
+    """
+    This method uses dhash.get_num_bits_different to find the difference
+    :param h1:
+    :param h2:
+    :return:
+    """
     return dhash.get_num_bits_different(h1, h2)
 
- 
+
 def processImage(csvfile):
+    """
+    This function passes messages to computeScore and diff functions to generate th result list with all the parameters.
+    It maintains a dictionary to save the hashes that are generated.
+
+    :param csvfile:
+    :return results:
+    """
     results = []
     start_time = 0
     dictHash = dict()
@@ -20,11 +37,17 @@ def processImage(csvfile):
         if row.__len__():
             start_time = time.process_time()
             score = computeScore(dictHash, row)
-            results.append([row[0], row[1], score, round(time.process_time() - start_time,3)])
+            results.append([row[0], row[1], score, round(time.process_time() - start_time, 3)])
     return results
 
 
 def computeScore(dictHash, row):
+    """
+    This function uses dhash module methods t0 generate hashes and compute the difference
+    :param dictHash:
+    :param row:
+    :return score:
+    """
     if row[0] not in dictHash.keys():
         dictHash[row[0]] = dhash.dhash_int(Image.open(row[0]))
     if row[1] not in dictHash.keys():
@@ -33,10 +56,11 @@ def computeScore(dictHash, row):
     score = round((difference / (8 * 8 * 2)), 2)
     return score
 
-def main(argvs):
 
-    if (argvs.__len__() <=1):
-        sys.exit("Error-> That looks like a wrong file format. The supported file formats are csv and txt with the data in the format of image1,image2")
+def main(argvs):
+    if (argvs.__len__() <= 1):
+        sys.exit(
+            "Error-> That looks like a wrong file format. The supported file formats are csv and txt with the data in the format of image1,image2")
     if (argvs[1].endswith(('.csv', '.txt'))):
         with open(argvs[1], encoding='utf-8-sig') as file:
             csvfile = csv.reader(file)
@@ -46,7 +70,8 @@ def main(argvs):
             thewriter = csv.writer(newfile)
             for i in range(0, result.__len__()):
                 thewriter.writerow(result[i])
-            print("\n The results of similarity can be found at "+ os. getcwd()+"\\comparison_result.csv\n"+" The data is in the format of \"image1,image2,score,elapsed\"")
+            print(
+                "\n The results of similarity can be found at " + os.getcwd() + "\\comparison_result.csv\n" + " The data is in the format of \"image1,image2,score,elapsed\"")
             newfile.close()
     else:
         sys.exit("Error-> That looks like a wrong file format. The supported file formats are csv and txt.")
